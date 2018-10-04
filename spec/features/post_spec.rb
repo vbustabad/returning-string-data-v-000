713574929c8126250f5af545af6c94f13dac2ce3
Veronica Bustabad <vbustabad@gmail.com>
2018-10-04 04:06:36 +0000

    Done.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 describe 'navigate' do
   before do
-    @post = Post.create(title: "My Post", description: "My post desc")
+    @author = Author.create(name: "Me", hometown: "The town where I lived.")
+    @post = Post.create(title: "My Post", description: "My post desc", author_id: @author.id)
   end
 
   it 'shows the title on the show page in a h1 tag' do
